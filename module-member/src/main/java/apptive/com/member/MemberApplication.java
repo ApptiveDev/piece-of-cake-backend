b780668271c93cebbd34af2fb620a8a18abb2e20
seasameoil <whdwlgus12@gmail.com>
2025-02-01 16:51:49 +0900

    fix (2)

--- module-member/src/main/java/apptive/com/member/MemberApplication.java
+++ module-member/src/main/java/apptive/com/member/MemberApplication.java
@@ -3,17 +3,15 @@ package apptive.com.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"apptive.com.member", "apptive.com.common"})
-@EntityScan(basePackages = {"apptive.com.member", "apptive.com.store.cake.model", "apptive.com.store.store.model"})
-@EnableJpaRepositories(basePackages = {"apptive.com.member",
+@EntityScan(basePackages = {"apptive.com.member.users.model", "apptive.com.store.cake.model", "apptive.com.store.store.model"})
+@EnableJpaRepositories(basePackages = {"apptive.com.member.users.repository",
         "apptive.com.store.cake.repository", "apptive.com.store.store.repository"})
 public class MemberApplication {
     public static void main(String[] args) {
